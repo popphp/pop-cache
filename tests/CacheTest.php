68cd8a4e92d516b2566e222d31e4b86ae5efa00b
Nick Sagona <nick@nolainteractive.com>
2015-06-11 16:56:43 -0500

    Start working on the unit tests

---
+++ tests/CacheTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pop\Acl\Test;
+
+use Pop\Cache\Cache;
+use Pop\Cache\Adapter;
+
+class CacheTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstructor()
+    {
+
+    }
+
+}
