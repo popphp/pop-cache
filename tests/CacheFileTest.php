e714a91aeadbf2567b31f49774596b7e115537bb
Nick Sagona <nick@nolainteractive.com>
2015-06-11 17:20:11 -0500

    Start working on unit tests

---
+++ tests/CacheFileTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pop\Acl\Test;
+
+use Pop\Cache\Adapter\File;
+
+class CacheFileTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstructor()
+    {
+        mkdir(__DIR__ . '/cache');
+        $cache = new File(__DIR__ . '/cache');
+        $this->assertInstanceOf('Pop\Cache\Adapter\File', $cache);
+        $this->assertEquals(__DIR__ . '/cache', $cache->getDir());
+        $cache->clear(true);
+        $this->assertFalse(file_exists(__DIR__ . '/cache'));
+
+    }
+
+    public function testConstructorException()
+    {
+        $this->setExpectedException('Pop\Cache\Adapter\Exception');
+        $cache = new File(__DIR__ . '/badcache');
+    }
+
+}
--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -10,7 +10,41 @@ class CacheTest extends \PHPUnit_Framework_TestCase
 
     public function testConstructor()
     {
+        mkdir(__DIR__ . '/cache');
+        $cache = new Cache(new Adapter\File(__DIR__ . '/cache'));
+        $cache->setLifetime(60);
+        $this->assertInstanceOf('Pop\Cache\Cache', $cache);
+        $this->assertInstanceOf('Pop\Cache\Adapter\File', $cache->adapter());
+        $this->assertEquals(60, $cache->getLifetime());
+        $this->assertTrue($cache->getAvailableAdapters()['file']);
+        $this->assertTrue($cache->isAvailable('file'));
+    }
 
+    public function testSaveAndLoad()
+    {
+        $cache = new Cache(new Adapter\File(__DIR__ . '/cache'));
+        $cache->save('foo', 'bar');
+        $this->assertEquals('bar', $cache->load('foo'));
+    }
+
+    public function testRemove()
+    {
+        $cache = new Cache(new Adapter\File(__DIR__ . '/cache'));
+        $cache->remove('foo');
+        $this->assertFalse($cache->load('foo'));
+    }
+
+    public function testClear()
+    {
+        $cache = new Cache(new Adapter\File(__DIR__ . '/cache'));
+        $cache->save('foo', 'bar');
+        $cache->save('baz', 123);
+        $this->assertEquals('bar', $cache->load('foo'));
+        $this->assertEquals(123, $cache->load('baz'));
+        $cache->clear();
+        $this->assertFalse($cache->load('foo'));
+        $this->assertFalse($cache->load('baz'));
+        rmdir(__DIR__ . '/cache');
     }
 
 }
